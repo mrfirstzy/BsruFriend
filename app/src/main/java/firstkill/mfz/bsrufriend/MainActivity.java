881276864494618cc9159235125feaec79dfac7d
MFz <tirapun2538@gmail.com>
2017-02-09 15:22:53 +0700

    event button

--- app/src/main/java/firstkill/mfz/bsrufriend/MainActivity.java
+++ app/src/main/java/firstkill/mfz/bsrufriend/MainActivity.java
@@ -1,13 +1,38 @@
 package firstkill.mfz.bsrufriend;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit การประการตัวแปร
+    private Button signInButton, signUPButton;
+    private EditText userEditText, passEditText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Bind Widget คือ การ Initial Var กับ View บน XML
+        signInButton = (Button) findViewById(R.id.button);
+        signUPButton = (Button) findViewById(R.id.button);
+        userEditText = (EditText) findViewById(R.id.editText);
+        passEditText = (EditText) findViewById(R.id.editText3);
+
+        //Button
+        signUPButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+            }
+        });
+
+    }   // Main Method
+
+}   // Main Class คลาสหลัก
